df2041e0841afe01a101cd82510e3a7e2e2c48bf
Brittanie Pham <brittaniepham@gmail.com>
2018-05-28 20:07:00 -0600

    form/show

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do
+  Note.create(
+    title: Faker::HowIMetYourMother.catch_phrase,
+    body: Faker::HowIMetYourMother.quote,
+  )
+end
+
+puts 'seeded'
